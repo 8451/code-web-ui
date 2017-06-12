48f82bd50363eb354273a10d0d463315f574e5f0
Jake Zarobsky <jake.zarobsky@8451.com>
2017-06-12 15:54:53 -0400

    Implemented service

--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -1,34 +1,51 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 import { Question } from '../question';
+import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class QuestionService {
 
+  // TODO: dynamically fill this based on the environment.
   private questionsUrl = 'http://localhost:8090/api/v1/questions';
 
   constructor(private http: Http) { }
 
-  getQuestions(): Promise<Question[]> {
+  getQuestions(): Observable<Question[]> {
     return this.http.get(this.questionsUrl)
-      .toPromise()
-      .then(response => response.json().questions as Question[])
-      .catch(e => {
-        console.error('An error occurred when retrieving questions', e);
-        return Promise.reject(e.message || e);
-      })
+      .map(res => res.json().questions)
+      .catch(this.handleError);
   }
 
-  getQuestion(id: string): Promise<Question> {
+  getQuestion(id: string): Observable<Question> {
     return this.http.get(`${this.questionsUrl}/${id}`)
-    .toPromise()
-    .then(response => response.json().question as Question)
-      .catch(e => {
-        console.error('An error occurred when trying to retrieve a question', e);
-        return Promise.reject(e.message || e);
-    });
+      .map(res => res.json().questions[0])
+      .catch(this.handleError);
+  }
+
+  createQuestion(question : Question): Observable<Question> {
+    return this.http.post(`${this.questionsUrl}`, question)
+      .map(res => res.json().questions[0])
+      .catch(this.handleError);
+  }
+
+  updateQuestion(question: Question): Observable<Question> {
+    return this.http.put(`${this.questionsUrl}`, question)
+      .map(res => res.json().questions[0])
+      .catch(this.handleError);
+  }
+
+  deleteQuestion(id: string): Observable<boolean> {
+    return this.http.delete(`${this.questionsUrl}/${id}`)
+      .map(res => true)
+      .catch(this.handleError);
+  }
+
+  handleError (error: Response | any) : Observable<string> {
+    // TODO: add alert error messages
+    return Observable.throw(error.statusText);
   }
 
 }
