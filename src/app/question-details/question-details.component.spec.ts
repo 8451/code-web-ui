c7c1d76f1f6a1b2d4bf938bc31dbf284a1e8c17e
Eric Hemphill <eric.hemphill@8451.com>
2017-06-19 10:13:38 -0400

    Added testing for question details component

--- src/app/question-details/question-details.component.spec.ts
+++ src/app/question-details/question-details.component.spec.ts
@@ -16,68 +16,100 @@ describe('QuestionDetailsComponent', () => {
   let questionService: QuestionService;
   let component: QuestionDetailsComponent;
   let fixture: ComponentFixture<QuestionDetailsComponent>;
-  const question: Question = {
+  let question: Question = {
+    'id': 'id1',
+    'title': 'Title1',
+    'difficulty': 2,
+    'body': 'Body1',
+    'suggestedAnswer': 'SuggestedAnswer1',
+    'createdBy': 'createdBy1',
+    'createdDate': null,
+    'modifiedBy': 'modifiedBy1',
+    'modifiedDate': null
+  };
+  let titleControl;
+  let bodyControl;
+  let answerControl;
+  let difficultyControl;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [AppModule,
+        RouterTestingModule,
+        FormsModule],
+      providers: [QuestionService,
+        { provide: ActivatedRoute, useValue: { url: Observable.of([{ path: 'new' }]) } }
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuestionDetailsComponent);
+    component = fixture.componentInstance;
+    question = {
       'id': 'id1',
       'title': 'Title1',
       'difficulty': 2,
       'body': 'Body1',
       'suggestedAnswer': 'SuggestedAnswer1',
       'createdBy': 'createdBy1',
-      'createdDate' : null,
+      'createdDate': null,
       'modifiedBy': 'modifiedBy1',
       'modifiedDate': null
-  };
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [AppModule,
-      RouterTestingModule,
-      FormsModule],
-      providers: [ QuestionService,
-        {provide: ActivatedRoute, useValue: {url: Observable.of([{path: 'new'}])}}
-      ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(QuestionDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    };
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
 
-  // TODO These tests aren't working correctly.  The form is always invalid
   it('should be an invalid form if the title is empty', async(() => {
-      questionService = fixture.debugElement.injector.get(QuestionService);
-      spy = spyOn(questionService, 'getQuestion').and.returnValue(Observable.of(question));
+    questionService = fixture.debugElement.injector.get(QuestionService);
+    spy = spyOn(questionService, 'getQuestion').and.returnValue(Observable.of(question));
 
-       fixture.whenStable().then(() => { // wait for async getQuestion
-        component.question = question;
-        component.question.title = ''; // Invalidates the title field
-        fixture.detectChanges();        // update question with the new title
-        expect(component.questionForm.valid).toBeFalsy();
-       }).catch((e) => {
-        expect(true).toBeFalsy();
-        console.error(e);
-      });
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => { // wait for async getQuestion
+      titleControl = component.questionForm.control.get('title');
+      bodyControl = component.questionForm.control.get('body');
+      answerControl = component.questionForm.control.get('suggestedAnswer');
+      difficultyControl = component.questionForm.control.get('difficulty');
+
+      fixture.detectChanges();
+
+      titleControl.setValue(question.title);
+      bodyControl.setValue(question.body);
+      answerControl.setValue(question.suggestedAnswer);
+      difficultyControl.setValue(question.difficulty);
+
+      titleControl.setValue(''); // Invalidates the title field
+      fixture.detectChanges();        // update question with the new title
+      expect(titleControl.valid).toBeFalsy();
+    }).catch((e) => {
+      expect(true).toBeFalsy();
+      console.error('Caught error inavlid form if title is empty', e);
+    });
   }));
 
   it('should be an invalid form if the question body is empty', async(() => {
     questionService = fixture.debugElement.injector.get(QuestionService);
     spy = spyOn(questionService, 'getQuestion').and.returnValue(Observable.of(question));
 
+    fixture.detectChanges();
+
     fixture.whenStable().then(() => {
-      component.question = question;
-      component.question.body = ''; // Invalidates the body field
+      bodyControl = component.questionForm.control.get('body');
+
       fixture.detectChanges();
-      expect(component.questionForm.valid).toBeFalsy();
+
+      bodyControl.setValue('');
+
+      fixture.detectChanges();
+      expect(bodyControl.valid).toBeFalsy();
     }).catch((e) => {
       expect(true).toBeFalsy();
-      console.error(e);
+      console.error('Caught error invalid form if body is empty', e);
     });
   }));
 
@@ -89,46 +121,28 @@ describe('QuestionDetailsComponent', () => {
     fixture.detectChanges();
 
     fixture.whenStable().then(() => {
-      component.question = question;
+      titleControl = component.questionForm.control.get('title');
+      bodyControl = component.questionForm.control.get('body');
+      answerControl = component.questionForm.control.get('suggestedAnswer');
+      difficultyControl = component.questionForm.control.get('difficulty');
 
-      // set the title FormControl
-      const titleControl = component.questionForm.control.get('title');
-      const bodyControl = component.questionForm.control.get('body');
-      const answerControl = component.questionForm.control.get('suggestedAnswer');
-      const difficultyControl = component.questionForm.control.get('difficulty');
-
-      expect(titleControl).toBeTruthy();
+      fixture.detectChanges();
 
       titleControl.setValue(question.title);
-      console.log('title ', titleControl.value);
       bodyControl.setValue(question.body);
       answerControl.setValue(question.suggestedAnswer);
       difficultyControl.setValue(question.difficulty);
 
       fixture.detectChanges();
+
       expect(titleControl.valid).toBe(true);
-      console.log('title valid: ' + titleControl.valid);
       expect(bodyControl.valid).toBe(true);
       expect(answerControl.valid).toBe(true);
       expect(difficultyControl.valid).toBe(true);
       expect(component.questionForm.valid).toBe(true);
     }).catch((e) => {
       expect(true).toBeFalsy();
-      console.error(e);
-    });
-  }));
-
-  it('should display the correct title in the #title element', async(() => {
-    questionService = fixture.debugElement.injector.get(QuestionService);
-    spy = spyOn(questionService, 'getQuestion').and.returnValue(Observable.of(question));
-
-    fixture.whenStable().then(() => {
-      component.question = question;
-      fixture.detectChanges();
-      expect(fixture.debugElement.nativeElement.querySelector('#title').textContent).toEqual(question.title);
-    }).catch((e) => {
-      expect(true).toBeFalsy();
-      console.error(e);
+      console.error('Caught error valid form', e);
     });
   }));
 
@@ -136,6 +150,7 @@ describe('QuestionDetailsComponent', () => {
     questionService = fixture.debugElement.injector.get(QuestionService);
     spy = spyOn(questionService, 'getQuestion').and.returnValue(Observable.of(question));
 
+    fixture.detectChanges();
     fixture.whenStable().then(() => {
       component.question = question;
       component.isNew = false;
@@ -149,31 +164,9 @@ describe('QuestionDetailsComponent', () => {
     });
   }));
 
-  it('should submit form when save button is clicked', async(() => {
-    questionService = fixture.debugElement.injector.get(QuestionService);
-    spy = spyOn(questionService, 'getQuestion').and.returnValue(Observable.of(question));
-    spyOn(component.questionForm, 'onSubmit');
-    spyOn(component, 'submitQuestion');
-    spyOn(component.questionForm, 'valid').and.returnValue(true);
-
-    fixture.whenStable().then(() => {
-      component.question = question;
-      component.isNew = false;
-      const button = fixture.debugElement.nativeElement.querySelector('#save-question-btn');
-      expect(button).toBeTruthy(); // Ensure the button exist
-      expect(button.isEnabled()).toBe(true); // the button is disabled, which is why the click event won't work
-      button.click();
-      fixture.detectChanges();
-      // expect(component.submitQuestion).toHaveBeenCalled();
-      // expect(component.questionForm.onSubmit).toHaveBeenCalled(); // Ensure the submit question has been called
-      // expect(component.questionForm.submitted).toBe(true);
-    }).catch((e) => {
-      expect(true).toBeFalsy();
-      console.error(e);
-    });
-  }));
-
   it('should display an add button when a question is modified', async(() => {
+    fixture.detectChanges();
+
     fixture.whenStable().then(() => {
       component.question = question;
       component.isNew = true; // Set the new property so the question is new
@@ -183,31 +176,14 @@ describe('QuestionDetailsComponent', () => {
       expect(button.textContent).toContain('Add'); // ensure the button says save
     }).catch((e) => {
       expect(true).toBeFalsy();
-      console.error(e);
-    });
-  }));
-
-  it('should submit the form when add button is clicked', async(() => {
-    spyOn(component.questionForm, 'onSubmit');
-
-    fixture.whenStable().then(() => {
-      component.question = question;
-      component.isNew = true; // Set the new property so the question is new
-      fixture.detectChanges();
-      const button = fixture.debugElement.nativeElement.querySelector('#add-question-btn');
-      expect(button).toBeTruthy(); // Ensure the button exists
-      expect(button.textContent).toContain('Add'); // ensure the button says save
-      button.click();
-      expect(component.questionForm.onSubmit).toHaveBeenCalled(); // Ensure the submit question has been called
-    }).catch((e) => {
-      expect(true).toBeFalsy();
-      console.error(e);
     });
   }));
 
   it('should call deleteQuestion when the delete button is pressed', async(() => {
     questionService = fixture.debugElement.injector.get(QuestionService);
-    spyOn(questionService, 'deleteQuestion');
+    spyOn(component, 'deleteQuestion');
+
+    fixture.detectChanges();
 
     fixture.whenStable().then(() => {
       component.question = question;
@@ -222,73 +198,67 @@ describe('QuestionDetailsComponent', () => {
     });
   }));
 
-  it('should call submitQuestion when a form is submitted', async(() => {
-    spyOn(component, 'submitQuestion');
+  it('should call createQuestion if a new question is submitted', async(() => {
+    questionService = fixture.debugElement.injector.get(QuestionService);
+    spyOn(questionService, 'createQuestion').and.returnValue(Observable.of(question));
+
+    fixture.detectChanges();
+
     fixture.whenStable().then(() => {
       component.question = question;
+      component.isNew = true;
+
+      titleControl = component.questionForm.control.get('title');
+      bodyControl = component.questionForm.control.get('body');
+      answerControl = component.questionForm.control.get('suggestedAnswer');
+      difficultyControl = component.questionForm.control.get('difficulty');
+
       fixture.detectChanges();
-      const form = fixture.debugElement.nativeElement.querySelector('questionForm');
-      expect(form).toBeTruthy();
-      // form.onSubmit();
+
+      titleControl.setValue(question.title);
+      bodyControl.setValue(question.body);
+      answerControl.setValue(question.suggestedAnswer);
+      difficultyControl.setValue(question.difficulty);
+
       fixture.detectChanges();
-      // expect(component.questionForm.submitted).toBe(true);
-      // expect(component.submitQuestion).toHaveBeenCalled();
+      component.submitQuestion();
+      expect(component.isNew).toBe(true);
+      expect(questionService.createQuestion).toHaveBeenCalled();
     }).catch((e) => {
       expect(true).toBeFalsy();
       console.error(e);
     });
   }));
 
-  it('should navigate back to /questions when a question is added', async(() => inject([Router], (router: Router) => {
+  it('should call updateQustion if an existing question is submitted', async(() => {
     questionService = fixture.debugElement.injector.get(QuestionService);
-    spy = spyOn(questionService, 'createQuestion').and.returnValue(Observable.of(question));
-    spyOn(router, 'navigate');
+    spyOn(questionService, 'updateQuestion').and.returnValue(Observable.of(question));
+
+    fixture.detectChanges();
 
     fixture.whenStable().then(() => {
       component.question = question;
-      component.isNew = true;
+      component.isNew = false;
+
+      titleControl = component.questionForm.control.get('title');
+      bodyControl = component.questionForm.control.get('body');
+      answerControl = component.questionForm.control.get('suggestedAnswer');
+      difficultyControl = component.questionForm.control.get('difficulty');
+
       fixture.detectChanges();
-      const form = fixture.debugElement.nativeElement.querySelector('questionForm');
-      form.onSubmit();
-      expect(router.navigate).toHaveBeenCalledWith(['/questions']);
+
+      titleControl.setValue(question.title);
+      bodyControl.setValue(question.body);
+      answerControl.setValue(question.suggestedAnswer);
+      difficultyControl.setValue(question.difficulty);
+
+      fixture.detectChanges();
+      component.submitQuestion();
+      expect(component.isNew).toBe(false);
+      expect(questionService.updateQuestion).toHaveBeenCalled();
     }).catch((e) => {
       expect(true).toBeFalsy();
       console.error(e);
     });
-  })));
-
-  it('should navigate back to /questions when a question is saved', async(() => inject([Router], (router: Router) => {
-    questionService = fixture.debugElement.injector.get(QuestionService);
-    spy = spyOn(questionService, 'updateQuestion').and.returnValue(Observable.of(question));
-    spyOn(router, 'navigate');
-
-    fixture.whenStable().then(() => {
-      component.question = question;
-      fixture.detectChanges();
-      const form = fixture.debugElement.nativeElement.querySelector('questionForm');
-      form.onSubmit();
-      expect(router.navigate).toHaveBeenCalledWith(['/questions']);
-    }).catch((e) => {
-      expect(true).toBeFalsy();
-      console.error(e);
-    });
-  })));
-
-  it('should navigate back to /questions when a question is deleted', async(() => inject([Router], (router: Router) => {
-    questionService = fixture.debugElement.injector.get(QuestionService);
-    spyOn(questionService, 'deleteQuestion');
-    spyOn(router, 'navigate');
-
-    fixture.whenStable().then(() => {
-      component.question = question;
-      fixture.detectChanges();
-      const deleteBtn = fixture.debugElement.nativeElement.querySelector('#delete-question-btn');
-      deleteBtn.click();
-      expect(component.deleteQuestion).toHaveBeenCalledWith(question.id);
-      expect(router.navigate).toHaveBeenCalledWith(['/questions']);
-    }).catch((e) => {
-      expect(true).toBeFalsy();
-      console.error(e);
-    });
-  })));
+  }));
 });
