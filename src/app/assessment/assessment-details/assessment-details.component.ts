ea947d48ed5789f378b9fae2651a74d0c73ec613
h428219 <holly.laiveling@8451.com>
2017-08-29 07:48:04 -0400

    Debugging build issues

--- src/app/assessment/assessment-details/assessment-details.component.ts
+++ src/app/assessment/assessment-details/assessment-details.component.ts
@@ -7,7 +7,7 @@ import { Assessment } from './../../domains/assessment';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { AssessmentService } from './../../services/assessment/assessment.service';
 import { Component, OnInit } from '@angular/core';
-import { IStarRatingOnRatingChangeEven } from 'angular-star-rating';
+import { OnRatingChangeEven } from 'angular-star-rating';
 
 @Component({
   selector: 'app-assessment-details',
@@ -63,7 +63,7 @@ export class AssessmentDetailsComponent implements OnInit {
     return editorTranslator(language);
   }
 
-  onRatingChange($event: IStarRatingOnRatingChangeEven) {
+  onRatingChange($event: OnRatingChangeEven) {
     this.assessment.rating = $event.rating;
   }
 
